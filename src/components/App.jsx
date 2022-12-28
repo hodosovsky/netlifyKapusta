3d92bb24140e917592275cb6a4d18b328ffdc236
maksymomelchuk <omelchukmaksym@gmail.com>
2022-12-28 09:33:05 +0200

    google

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ import { PublicRoute } from './PublicRoute/PublicRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useMatchMedia } from 'hooks/use-match-media';
+import { BrowserRouter } from 'react-router-dom';
 
 const ExpensesPage = lazy(() => import('../pages/ExpensesPage/ExpensesPage'));
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
@@ -24,6 +25,24 @@ const ThereIsNoSuchPage = lazy(() =>
 
 export const App = () => {
   const dispatch = useDispatch();
+
+  // Google
+  const location = window.location;
+  const urlSearchParams = new URLSearchParams(location.search);
+  const accessToken = urlSearchParams.get('accessToken');
+
+  useEffect(() => {
+    if (accessToken) {
+      setAuthHeader(accessToken);
+      dispatch(addAccessToken(accessToken));
+      dispatch(refreshUser());
+      if (location.pathname === '/') {
+        location.pathname = '/kapusta/home';
+        dispatch(refreshUser());
+      }
+    }
+  }, [accessToken, dispatch, location]);
+  //
 
   const token = useSelector(selectToken);
 
@@ -41,39 +60,41 @@ export const App = () => {
   return (
     !isFetchingUser && (
       <>
-        <ToastContainer />
-        <Routes>
-          <Route path="/" element={<SharedLayouts />}>
-            <Route path="/" element={<PrivateRoute />}>
-              <Route index element={<Navigate to="/home" />} />
-              {!isMobile && (
-                <>
-                  <Route path="/home" element={<HomePage />}>
-                    <Route index element={<Navigate to="/home/expenses" />} />
-                    <Route path="income" element={<IncomePage />} />
-                    <Route path="expenses" element={<ExpensesPage />} />
-                  </Route>
-                </>
-              )}
-              {isMobile && (
-                <>
-                  <Route path="/home" element={<HomePage />} />
-                  <Route path="/income" element={<IncomePage />} />
-                  <Route path="/expenses" element={<ExpensesPage />} />
-                  <Route path="*" element={<Navigate to="/home" />} />
-                </>
-              )}
-              <Route path="/reports" element={<ReportsPage />} />
-            </Route>
-            <Route path="/" element={<PublicRoute />}>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegiserPage />} />
-              <Route path="*" element={<Navigate to="/login" />} />
-            </Route>
+        <BrowserRouter basename="/kapusta">
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<SharedLayouts />}>
+              <Route path="/" element={<PrivateRoute />}>
+                <Route index element={<Navigate to="/home" />} />
+                {!isMobile && (
+                  <>
+                    <Route path="/home" element={<HomePage />}>
+                      <Route index element={<Navigate to="/home/expenses" />} />
+                      <Route path="income" element={<IncomePage />} />
+                      <Route path="expenses" element={<ExpensesPage />} />
+                    </Route>
+                  </>
+                )}
+                {isMobile && (
+                  <>
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/income" element={<IncomePage />} />
+                    <Route path="/expenses" element={<ExpensesPage />} />
+                    <Route path="*" element={<Navigate to="/home" />} />
+                  </>
+                )}
+                <Route path="/reports" element={<ReportsPage />} />
+              </Route>
+              <Route path="/" element={<PublicRoute />}>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegiserPage />} />
+                <Route path="*" element={<Navigate to="/login" />} />
+              </Route>
 
-            <Route path="*" element={<ThereIsNoSuchPage />} />
-          </Route>
-        </Routes>
+              <Route path="*" element={<ThereIsNoSuchPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
         {/* <LightModalWindow>Are you sure?</LightModalWindow> */}
         {/* <Summary /> */}
         {/* <Form /> */}
